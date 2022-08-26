b281d72f03dfa8b2effd6285aea30bd230fb5773
rfa246 <reemafayaz00@gmail.com>
2022-08-26 09:28:15 -0400

    Added one test

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,29 @@
+import Pages.LoginPage;
+import browser.Browser;
+import browser.Info;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class FirstTest extends TestBase{
+   //WebDriver driver;
+
+    @Test
+    public void test01(){
+       startLoginPage().loginToPIMPage();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void test02(){
+
+    }
+}
